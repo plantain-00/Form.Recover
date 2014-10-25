3eff046f7c9818f268e460ca0ed3926f4483edfd
yaoyao <yaoyao12306@163.com>
2014-10-25 17:39:21 +0800

    v 1.0.0

---
+++ Form.Recover.Test/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+
+using HtmlAgilityPack;
+
+using Newtonsoft.Json.Linq;
+
+namespace Form.Recover.Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            ValidateRequest = false;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Save(string data, string html)
+        {
+            var jObject = JObject.Parse(data);
+            html = "<!DOCTYPE html>" + html;
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            doc.SetText("textId1", jObject);
+            doc.SetText<double>("textId2", jObject);
+            doc.SetCheckbox("checkboxId", jObject);
+            doc.SetRadiobox("radioId1", jObject, "radioId1", "radioId2");
+            doc.SetSelect("selectId", jObject);
+            html = doc.DocumentNode.WriteTo();
+
+            Session["html"] = html;
+            return new EmptyResult();
+        }
+
+        public ActionResult Recover()
+        {
+            return View();
+        }
+    }
+}
---
+++ Form.Recover/HtmlDocumentExtension.cs
@@ -0,0 +1,205 @@
+﻿using System.Linq;
+
+using Bootstrap.Pagination;
+
+using HtmlAgilityPack;
+
+using Newtonsoft.Json.Linq;
+
+namespace Form.Recover
+{
+    /// <summary>
+    ///     HtmlDocument的扩展，来自HtmlAgilityPack，用于从JToken中获取值，并赋值给表单作为值。
+    /// </summary>
+    public static class HtmlDocumentExtension
+    {
+        /// <summary>
+        ///     设置Text Input的value
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        public static void SetText(this HtmlDocument doc, string id, JToken jToken)
+        {
+            doc.SetText(id, jToken, id);
+        }
+
+        /// <summary>
+        ///     设置Text Input的value
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetText(this HtmlDocument doc, string id, JToken jToken, string name)
+        {
+            doc.GetElementbyId(id).SetAttributeValue("value", jToken.Get(name));
+        }
+
+        /// <summary>
+        ///     设置Text Input的value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        public static void SetText<T>(this HtmlDocument doc, string id, JToken jToken)
+        {
+            doc.SetText<T>(id, jToken, id);
+        }
+
+        /// <summary>
+        ///     设置Text Input的value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetText<T>(this HtmlDocument doc, string id, JToken jToken, string name)
+        {
+            doc.GetElementbyId(id).SetAttributeValue("value", jToken.Get<T>(name).ToString());
+        }
+
+        /// <summary>
+        ///     设置Select的当前选中项
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        public static void SetSelect(this HtmlDocument doc, string id, JToken jToken)
+        {
+            doc.SetSelect(id, jToken, id);
+        }
+
+        /// <summary>
+        ///     设置Select的当前选中项
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetSelect(this HtmlDocument doc, string id, JToken jToken, string name)
+        {
+            if (jToken[name] == null)
+            {
+                return;
+            }
+            var value = jToken.Get(name);
+            foreach (var node in doc.GetElementbyId(id).ChildNodes)
+            {
+                if (node.GetAttributeValue("value", "") == value)
+                {
+                    node.SetAttributeValue("selected", "selected");
+                }
+                else
+                {
+                    var attribute = node.Attributes.FirstOrDefault(a => a.Name == "selected");
+                    if (attribute != null)
+                    {
+                        attribute.Remove();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///     设置Checkbox Input的选中状态
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        public static void SetCheckbox(this HtmlDocument doc, string id, JToken jToken)
+        {
+            doc.SetCheckbox(id, jToken, id);
+        }
+
+        /// <summary>
+        ///     设置Checkbox Input的选中状态
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetCheckbox(this HtmlDocument doc, string id, JToken jToken, string name)
+        {
+            var isChecked = jToken.Get<bool>(name);
+            if (isChecked)
+            {
+                doc.GetElementbyId(id).SetAttributeValue("checked", "checked");
+            }
+        }
+
+        /// <summary>
+        ///     设置两个Radiobox Input的选中状态
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="id"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        /// <param name="anotherId"></param>
+        public static void SetRadiobox(this HtmlDocument doc, string id, JToken jToken, string name, string anotherId)
+        {
+            var isChecked = jToken.Get<bool>(name);
+            if (isChecked)
+            {
+                doc.GetElementbyId(id).SetAttributeValue("checked", "checked");
+                var attribute = doc.GetElementbyId(anotherId).Attributes.FirstOrDefault(a => a.Name == "checked");
+                if (attribute != null)
+                {
+                    attribute.Remove();
+                }
+            }
+            else
+            {
+                doc.GetElementbyId(anotherId).SetAttributeValue("checked", "checked");
+                var attribute = doc.GetElementbyId(id).Attributes.FirstOrDefault(a => a.Name == "checked");
+                if (attribute != null)
+                {
+                    attribute.Remove();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     设置Text Input的value
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="xpath"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetTextByXPath(this HtmlDocument doc, string xpath, JToken jToken, string name)
+        {
+            doc.DocumentNode.SelectSingleNode(xpath).SetAttributeValue("value", jToken.Get(name));
+        }
+
+        /// <summary>
+        ///     设置Text Input的value
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="xpath"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetTextByXPath<T>(this HtmlDocument doc, string xpath, JToken jToken, string name)
+        {
+            doc.DocumentNode.SelectSingleNode(xpath).SetAttributeValue("value", jToken.Get<T>(name).ToString());
+        }
+
+        /// <summary>
+        ///     设置Checkbox Input的选中状态
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="xpath"></param>
+        /// <param name="jToken"></param>
+        /// <param name="name"></param>
+        public static void SetCheckboxByXPath(this HtmlDocument doc, string xpath, JToken jToken, string name)
+        {
+            var isChecked = jToken.Get<bool>(name);
+            if (isChecked)
+            {
+                doc.DocumentNode.SelectSingleNode(xpath).SetAttributeValue("checked", "checked");
+            }
+        }
+    }
+}
